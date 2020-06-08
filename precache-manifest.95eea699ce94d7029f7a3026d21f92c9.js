6c78f44fa70c667e4685446f90cdba5f8476344f
jieunHan93 <jieunellenhan@gmail.com>
2020-06-08 23:14:08 +0900

    Updates

---
+++ precache-manifest.95eea699ce94d7029f7a3026d21f92c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27aea42ed49cf0932611cfe3f2523f5d",
+    "url": "/react-tictactoc/index.html"
+  },
+  {
+    "revision": "027ddaae27fa66488a8e",
+    "url": "/react-tictactoc/static/css/main.9d4cf694.chunk.css"
+  },
+  {
+    "revision": "02bccd38a7b659292444",
+    "url": "/react-tictactoc/static/js/2.398eef85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoc/static/js/2.398eef85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027ddaae27fa66488a8e",
+    "url": "/react-tictactoc/static/js/main.43c6ec60.chunk.js"
+  },
+  {
+    "revision": "0a069a1f9af5b57ad651",
+    "url": "/react-tictactoc/static/js/runtime-main.24df522d.js"
+  }
+]);
